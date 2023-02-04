7dce0873967483db50f58be003651b72297a7f15
AlyLoutfy <alyloutfy99@gmail.com>
2023-02-04 14:00:18 +0200

    Temp Conversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  return Math.round(5/9 * (deg-32) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  return Math.round(((5/9 * deg) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
