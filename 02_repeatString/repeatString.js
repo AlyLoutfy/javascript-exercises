5d4ef275a49a35f305e080c20459b98f9eef5466
AlyLoutfy <alyloutfy99@gmail.com>
2023-02-04 13:03:30 +0200

    Repeat String Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
+  let finalMsg = "";
+  for(let i = 0; i < num; i++) {
+    finalMsg += str;
+  }
+  return finalMsg;
 };
 
 // Do not edit below this line
