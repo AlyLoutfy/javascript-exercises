2a9edede1f317291c58ea93e32817685774f35c7
AlyLoutfy <alyloutfy99@gmail.com>
2023-02-07 11:25:48 +0200

    Calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total = 0;
+  for (let i = 0; i < a.length; i++) {
+    total += a[i];
+  }
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(a) {
+  let total;
+  if(a.length == 0) {
+    total = 0;
+    return total;
+  }
 
+  total = a[0];
+  for (let i = 1; i < a.length; i++) {
+    total *= a[i];
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if (a === 0 || a === 1) return 1;
 	
+  for (let i = a - 1; i >= 1; i--) {
+    a = a * i;
+  }
+  return a;
 };
 
 // Do not edit below this line
