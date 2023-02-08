b0e511b8989c2c12fb86c5f0a5d19626311bbe2a
AlyLoutfy <alyloutfy99@gmail.com>
2023-02-08 09:53:33 +0200

    Book Titles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+  let finalBooks = [];
 
+  for (let i = 0; i < books.length; i++) {
+    finalBooks[i] = books[i].title;
+  }
+  return finalBooks;
 };
 
 // Do not edit below this line
